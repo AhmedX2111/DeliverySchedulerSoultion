b480fbc8e6e7f1ba6b1f8bdb746d24d08b754b2b
AhmedX2111 <Ahmedkhaled910000@gmail.com>
2025-06-03 05:01:56 +0300

    Add Application Layer

---
+++ DeliveryScheduler.Application/UseCases/GetAvailableDeliverySlots.cs
@@ -0,0 +1,30 @@
+﻿using DeliveryScheduler.Domain.Entities;
+using DeliveryScheduler.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryScheduler.Application.UseCases
+{
+	public class GetAvailableDeliverySlots
+	{
+		private readonly IProductRepository _productRepo;
+		private readonly IDeliverySlotService _slotService;
+
+		public GetAvailableDeliverySlots(IProductRepository repo, IDeliverySlotService slotService)
+		{
+			_productRepo = repo;
+			_slotService = slotService;
+		}
+
+		public async Task<List<DeliverySlot>> ExecuteAsync(List<Guid> productIds, DateTime now)
+		{
+			var products = await _productRepo.GetProductsByIdsAsync(productIds);
+			var earliest = _slotService.GetEarliestDeliveryDate(products, now);
+			var slots = _slotService.GenerateSlots(earliest, now, products);
+			return slots;
+		}
+	}
+}
